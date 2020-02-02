27744d355a5e3954a26e72f604e85fa962f11c6b
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2020-02-02 09:35:01 -0300

    Correções e melhorias.

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,2 +1,16 @@
 const app = require('../app')
-app.listen(3000)
+const numCpus = require('os').cpus().length
+console.log({numCpus})
+const cluster = require('cluster')
+
+// pm2 / forever
+
+if (cluster.isMaster) {
+    for (let i = 0; i < numCpus; i++) {
+        cluster.fork()
+    }
+} else {
+    app.listen(3000, () => {
+        console.log('server is up')
+    })
+}
